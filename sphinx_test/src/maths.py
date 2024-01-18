6f19f6b327c3a40d299f438d0beace84badd7620
Oslolosen <Oslolosen@kali.kali>
2024-01-18 12:46:50 +0100

    Test av pipeline navn for DEV

---
+++ sphinx_test/src/maths.py
@@ -0,0 +1,14 @@
+def add_numbers(a, b):
+    """
+    Adds two numbers together.
+
+    Parameters:
+    a (int): The first number.
+    b (int): The second number.
+
+    Returns:
+    int: The sum of the two numbers.
+    """
+    return a + b
+
+
